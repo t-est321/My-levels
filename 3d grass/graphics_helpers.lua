7ffac025f5fac4b8f900f2702de228fe889e8925
t-est321 <otest3l2l1@gmail.com>
2026-07-19 02:31:00 +0400

    Add files via upload

---
+++ 3d grass/graphics_helpers.lua
@@ -0,0 +1,28 @@
+local helper = {}
+function helper.new_mesh()
+  local mesh = {}
+  mesh.vertexes = {}
+  mesh.segments = {}
+  mesh.colors = {}
+  return mesh
+end
+function helper.add_line_to_mesh(mesh,vertexes,colors,close_loop)
+  local vertex_count = #mesh.vertexes
+  local color_count = #mesh.colors
+  local segment_count = #mesh.segments
+  local number_of_new_segments = #vertexes-1
+  local segments = {}
+  for i = 1,#vertexes do
+    table.insert(mesh.vertexes,vertexes[i])
+    table.insert(mesh.colors,colors[i])
+  end
+  table.insert(segments,vertex_count)
+  for i = 1,number_of_new_segments do
+    table.insert(segments,vertex_count+i)
+  end
+  if close_loop then
+    table.insert(segments,vertex_count)
+  end
+  table.insert(mesh.segments,segments)
+end
+return helper
